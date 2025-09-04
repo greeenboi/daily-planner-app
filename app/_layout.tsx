7f631da1349709db8d8820feef13830b2d926e42
greeenboi <suvan.gowrishanker.204@gmail.com>
2025-09-05 02:00:01 +0530

    feat: initialized gluestack ui lib

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,17 @@ import { Stack } from "expo-router";
 
 import { authClient } from "@/lib/auth-client";
 
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/global.css';
+
 export default function RootLayout() {
   const { data: session } = authClient.useSession();
   const isAuthenticated = !!session;
 
   return (
-    <Stack screenOptions={{ headerShown: false }}>
+    
+    <GluestackUIProvider mode="light">
+      <Stack screenOptions={{ headerShown: false }}>
       <Stack.Protected guard={isAuthenticated}>
         <Stack.Screen name="(app)" />
       </Stack.Protected>
@@ -15,5 +20,7 @@ export default function RootLayout() {
         <Stack.Screen name="(auth)" />
       </Stack.Protected>
     </Stack>
+    </GluestackUIProvider>
+  
   );
 }
