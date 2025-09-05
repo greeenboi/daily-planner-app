a32817b294c93bb85cf088762a356d01e7d314c4
greeenboi <suvan.gowrishanker.204@gmail.com>
2025-09-05 12:31:49 +0530

    feat: added biome, formatting

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,71 +1,71 @@
+import { authClient } from "@/lib/auth-client";
 import { useState } from "react";
 import {
-  TextInput,
-  Button,
-  ScrollView,
-  Text,
-  StyleSheet,
-  Alert,
+	Alert,
+	Button,
+	ScrollView,
+	StyleSheet,
+	Text,
+	TextInput,
 } from "react-native";
-import { authClient } from "@/lib/auth-client";
 
 export default function SignUp() {
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
+	const [email, setEmail] = useState("");
+	const [name, setName] = useState("");
+	const [password, setPassword] = useState("");
 
-  const handleSignUp = async () => {
-    console.info("SIGN UP PRESS FUNC")
-    const signUpResponse = await authClient.signUp.email({
-      email,
-      password,
-      name,
-    });
-    if (signUpResponse.error) {
-      Alert.alert("Error", signUpResponse.error.message);
-      return;
-    }
-    console.info(signUpResponse.data)
-    console.info("NO ERRORS SIGNING UP")
-  };
+	const handleSignUp = async () => {
+		console.info("SIGN UP PRESS FUNC");
+		const signUpResponse = await authClient.signUp.email({
+			email,
+			password,
+			name,
+		});
+		if (signUpResponse.error) {
+			Alert.alert("Error", signUpResponse.error.message);
+			return;
+		}
+		console.info(signUpResponse.data);
+		console.info("NO ERRORS SIGNING UP");
+	};
 
-  return (
-    <ScrollView contentInsetAdjustmentBehavior="automatic">
-      <Text style={{ fontSize: 32, fontWeight: "bold", margin: 10 }}>
-        Sign Up
-      </Text>
-      <TextInput
-        placeholder="Name"
-        value={name}
-        onChangeText={setName}
-        style={styles.input}
-      />
-      <TextInput
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-        inputMode="email"
-        autoCapitalize="none"
-        style={styles.input}
-      />
-      <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        style={styles.input}
-        secureTextEntry
-      />
-      <Button title="Sign up" onPress={handleSignUp} />
-    </ScrollView>
-  );
+	return (
+		<ScrollView contentInsetAdjustmentBehavior="automatic">
+			<Text style={{ fontSize: 32, fontWeight: "bold", margin: 10 }}>
+				Sign Up
+			</Text>
+			<TextInput
+				placeholder="Name"
+				value={name}
+				onChangeText={setName}
+				style={styles.input}
+			/>
+			<TextInput
+				placeholder="Email"
+				value={email}
+				onChangeText={setEmail}
+				inputMode="email"
+				autoCapitalize="none"
+				style={styles.input}
+			/>
+			<TextInput
+				placeholder="Password"
+				value={password}
+				onChangeText={setPassword}
+				style={styles.input}
+				secureTextEntry
+			/>
+			<Button title="Sign up" onPress={handleSignUp} />
+		</ScrollView>
+	);
 }
 
 const styles = StyleSheet.create({
-  input: {
-    borderWidth: 1,
-    borderColor: "gray",
-    borderRadius: 10,
-    padding: 10,
-    margin: 10,
-  },
+	input: {
+		borderWidth: 1,
+		borderColor: "gray",
+		borderRadius: 10,
+		padding: 10,
+		margin: 10,
+	},
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,25 +2,23 @@ import { Stack } from "expo-router";
 
 import { authClient } from "@/lib/auth-client";
 
-import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
-import '@/global.css';
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import "@/global.css";
 
 export default function RootLayout() {
-  const { data: session } = authClient.useSession();
-  const isAuthenticated = !!session;
+	const { data: session } = authClient.useSession();
+	const isAuthenticated = !!session;
 
-  return (
-    
-    <GluestackUIProvider mode="light">
-      <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Protected guard={isAuthenticated}>
-        <Stack.Screen name="(app)" />
-      </Stack.Protected>
-      <Stack.Protected guard={!isAuthenticated}>
-        <Stack.Screen name="(auth)" />
-      </Stack.Protected>
-    </Stack>
-    </GluestackUIProvider>
-  
-  );
+	return (
+		<GluestackUIProvider mode="light">
+			<Stack screenOptions={{ headerShown: false }}>
+				<Stack.Protected guard={isAuthenticated}>
+					<Stack.Screen name="(app)" />
+				</Stack.Protected>
+				<Stack.Protected guard={!isAuthenticated}>
+					<Stack.Screen name="(auth)" />
+				</Stack.Protected>
+			</Stack>
+		</GluestackUIProvider>
+	);
 }
