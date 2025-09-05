ec4c2336ac61cd3a1935daff24b72fa03813a21c
greeenboi <suvan.gowrishanker.204@gmail.com>
2025-09-05 19:30:54 +0530

    feat: add BackgroundImage component to display a background SVG

---
+++ components/background-image.tsx
@@ -0,0 +1,20 @@
+import { Image } from "expo-image";
+
+export default function BackgroundImage() {
+	return (
+		<Image
+			source={require("../assets/background.svg")}
+			style={{
+				position: "absolute",
+				left: 0,
+				right: 0,
+				bottom: 0,
+				width: "100%",
+				height: "100%",
+				zIndex: 0,
+			}}
+			contentFit="fill"
+			pointerEvents="none"
+		/>
+	);
+}
